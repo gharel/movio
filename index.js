6d513ee0a2a2148adb355efd147329bc1c967c48
gharel <harel.guillaume@live.fr>
2017-02-01 05:27:50 +0100

    Refactor code and add comments

--- index.js
+++ index.js
@@ -1,321 +1,362 @@
-'use strict';
+(function () {
+	'use strict';
 
-// TODO IndexedDB to store movie
-// TODO Comments
-// TODO SASS
-// TODO BrowserSync
-// TODO Details page for a movie
-// TODO Filter by genre, actor, director
-// TODO Launch with vlc or select in explorer
+	// TODO IndexedDB to store movie
+	// TODO SASS
+	// TODO BrowserSync
+	// TODO Details page for a movie
+	// TODO Filter by genre, rating, year, actor, director
+	// TODO Launch movie with vlc or open explorer and select it
 
-const path = require('path');
-const fs = require('fs');
-const gui = require('nw.gui');
-const pug = require('pug');
-const imdb = require('imdb-api');
-const allocine = require('allocine-api');
+	const path = require('path');
+	const fs = require('fs');
+	const gui = require('nw.gui');
+	const pug = require('pug');
+	const imdb = require('imdb-api');
+	const allocine = require('allocine-api');
+	const win = gui.Window.get();
+	const console = win.window.console;
 
-let win = gui.Window.get();
-let console = win.window.console;
-let movies = [];
-let moviespath = [];
-let apiused = "imdb"; // imdb or allocine
-let sortasc = true;
-let fallbackapi = true;
-let defaultindex = 0;
-let flag = false;
-let sortused = "title"; // title, year or rating
-let folder;
-let folderpath;
+	// store movie infos from API
+	let moviesinfos = [];
+	// store movie path
+	let moviesfiles = [];
+	let options = {
+		api: "imdb",
+		folder: null,
+		orderby: "title",
+		order: "asc",
+		fallback: true,
+		extensions: [".avi", ".mkv", ".mp4"],
+		log: true
+	};
+	// store if application is occupy
+	let working = false;
 
-function getUserHome() {
-	let folder = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-	if (process.platform === 'win32') {
-		folder += '\\Downloads';
+	// initialize application
+	function init() {
+		if (!options.log) {
+			// in production, we can remove console.log with options.log set to false
+			console.log = function () {
+			};
+		}
+
+		window.onload = function () {
+			getConfig();
+			renderLayout();
+
+			getDefaultFolder();
+			getAllMoviesFiles(options.folder);
+		};
 	}
-	return folder;
-}
 
-function compare(a, b) {
-	if (a[sortused] < b[sortused]) {
-		return -1;
-	} else {
+	// get options form localStorage
+	function getConfig() {
+		for (let i in localStorage) {
+			if (localStorage.hasOwnProperty(i)) {
+				options[i] = localStorage.getItem(i);
+
+				// convert options from localstorage to retrieve their real type and avoid error when used options
+				if (options[i] === "true") {
+					options[i] = true;
+				} else if (options[i] === "false") {
+					options[i] = false;
+				}
+				// convert extensions from string to array
+				if (i === "extensions") {
+					options[i] = options[i].split(",");
+				}
+			}
+		}
+	}
+
+	// set options of application to localStorage to find them on later use
+	function setConfig() {
+		for (let i in options) {
+			if (options.hasOwnProperty(i)) {
+				localStorage.setItem(i, options[i]);
+			}
+		}
+	}
+
+	// use in sortArray to compare infos to ordering them
+	function compare(a, b) {
+		if (a[options.orderby] < b[options.orderby]) {
+			return -1;
+		}
 		return 1;
 	}
-}
 
-function sort(array, asc) {
-	let temp = array.sort(compare);
-	if (!asc) {
-		temp = array.reverse();
+	// use in renderMovie to order movie
+	function sortArray(array, order) {
+		let tmp = array.sort(compare);
+		if (order === "desc") {
+			tmp = array.reverse();
+		}
+		return tmp;
 	}
-	return temp;
-}
 
-function renderLayout() {
-	pug.renderFile('layout.pug', {}, (err, res) => {
-		document.getElementById("movio").innerHTML = res;
+	// generate HTML layout of application and add listener for menu item
+	function renderLayout() {
+		pug.renderFile('layout.pug', {}, (err, res) => {
+			setConfig();
 
-		localStorage.setItem("currentfolder", folder);
-		document.querySelector('.js-open').addEventListener('click', (event) => {
-			openFolderModal();
+			document.getElementById("movio").innerHTML = res;
+
+			document.querySelector('.js-open').addEventListener('click', () => {
+				openNewFolder();
+			});
+
+			document.querySelector('.js-sort').addEventListener('click', () => {
+				sortMovies();
+			});
+
+			document.querySelector('.js-api').addEventListener('click', () => {
+				changeApi();
+			});
 		});
+	}
 
-		localStorage.setItem("sortby", sortused);
-		localStorage.setItem("asc", sortasc);
-		document.querySelector('.js-sort').addEventListener('click', (event) => {
-			if (sortused === "title" && !sortasc) {
-				sortused = "year";
-			} else if (sortused === "year" && !sortasc) {
-				sortused = "rating";
-			} else if (sortused === "rating" && !sortasc) {
-				sortused = "title"
-			}
+	// generate HTML of movies and listen click on movie
+	function renderMovie() {
+		let el;
+		sortArray(moviesinfos, options.order);
 
-			sortasc = !sortasc;
-			localStorage.setItem("sortby", sortused);
-			localStorage.setItem("asc", sortasc);
+		pug.renderFile('movies.pug', {"movies": moviesinfos}, (err, res) => {
+			working = false;
+			document.getElementById("js-main").innerHTML = res;
 
-			renderMovie();
-		});
-
-		localStorage.setItem("currentapi", apiused);
-		localStorage.setItem("apifallback", fallbackapi);
-		document.querySelector('.js-api').addEventListener('click', (event) => {
-			let pending;
-
-			if (apiused === "imdb") {
-				apiused = "allocine";
-			} else {
-				apiused = "imdb";
-			}
-
-			localStorage.setItem("currentapi", apiused);
-			movies = [];
-
-			pending = moviespath.length;
-			moviespath.forEach((element) => {
-				getMovie(element, (err, res) => {
-					pending--;
-					movies.push(res);
-					if (!pending) {
-						renderMovie();
-					}
+			el = document.querySelectorAll('.movie__content');
+			el.forEach((element) => {
+				element.addEventListener('click', (event) => {
+					console.log(event.target);
 				});
 			});
 		});
-	});
-}
+	}
 
-function openFolderModal() {
-	let inputFolder = document.querySelector('.field-folder');
-	inputFolder.addEventListener('change', (event) => {
-		if (!flag) {
-			flag = true;
-			folderpath = event.target.value;
-			localStorage.setItem("currentfolder", folderpath);
-			readdirectory(folderpath);
+	// open new folder with a system modal thanks to input file
+	function openNewFolder() {
+		let input = document.querySelector('.field-folder');
+		if (!working) {
+			working = true;
+			input.addEventListener('change', (event) => {
+				options.folder = event.target.value;
+				localStorage.setItem("folder", options.folder);
+
+				getAllMoviesFiles(options.folder);
+			});
+			input.click();
 		}
-	});
-	inputFolder.click();
-}
+	}
 
-function renderMovie() {
-	let el;
-	sort(movies, sortasc);
+	// change order and orderby options and render movies after that
+	function sortMovies() {
+		if (options.orderby === "title" && options.order === "desc") {
+			options.orderby = "year";
+		} else if (options.orderby === "year" && options.order === "desc") {
+			options.orderby = "rating";
+		} else if (options.orderby === "rating" && options.order === "desc") {
+			options.orderby = "title"
+		}
 
-	console.log(movies);
+		if (options.order === "asc") {
+			options.order = "desc";
+		} else {
+			options.order = "asc";
+		}
 
-	pug.renderFile('movies.pug', {"movies": movies}, (err, res) => {
-		document.getElementById("js-main").innerHTML = res;
+		localStorage.setItem("orderby", options.orderby);
+		localStorage.setItem("order", options.order);
 
-		el = document.querySelectorAll('.movie__content');
-		el.forEach((element) => {
-			element.addEventListener('click', (event) => {
-				console.log(event.target);
+		renderMovie();
+	}
+
+	// change API used to retrieve movie's data
+	function changeApi() {
+		if (options.api === "imdb") {
+			options.api = "allocine";
+		} else {
+			options.api = "imdb";
+		}
+
+		localStorage.setItem("api", options.api);
+
+		getAllMoviesInfos();
+	}
+
+	// define default folder with downloads directory for windows or home directory for others
+	function getDefaultFolder() {
+		let folder = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
+		if (process.platform === 'win32') {
+			folder += '\\Downloads';
+		}
+		options.folder = folder;
+		localStorage.setItem('folder', options.folder);
+	}
+
+	// get movies path into an array
+	function getAllMoviesFiles(folder) {
+		moviesfiles = [];
+
+		readFolder(folder, (err, res) => {
+			filterMoviesFiles(res, (err, res) => {
+				moviesfiles = res;
+				getAllMoviesInfos();
 			});
 		});
-	});
-}
+	}
 
-function filtermovies(array, callback) {
-	array = array.filter((element) => {
-		if (path.extname(element) === ".avi" || path.extname(element) === ".mkv" || path.extname(element) === ".mp4") {
-			return element;
-		}
-	});
-	return callback(null, array);
-}
-
-function readfolder(folder, callback) {
-	let results = [];
-	fs.readdir(folder, (err, files) => {
+	// get all movies infos form movies path array
+	function getAllMoviesInfos() {
 		let pending;
-		if (err) {
-			return callback(err);
-		}
-		pending = files.length;
-		if (!pending) {
-			return callback(null, results);
-		}
-		files.forEach((file) => {
-			file = path.resolve(folder, file);
-			fs.stat(file, (err, stat) => {
-				if (stat && stat.isDirectory()) {
-					readfolder(file, (err, res) => {
-						pending--;
-						results = results.concat(res);
-						if (!pending) {
-							callback(null, results);
-						}
-					});
-				} else {
-					results.push(file);
-					pending--;
-					if (!pending) {
-						callback(null, results);
-					}
+		moviesinfos = [];
+		pending = moviesfiles.length;
+		moviesfiles.forEach((element) => {
+			getMovieInfos(element, (err, res) => {
+				pending--;
+				moviesinfos.push(res);
+				if (!pending) {
+					renderMovie();
 				}
 			});
 		});
-	});
-}
-
-function getMovie(filepath, callback) {
-	let result = {};
-	result.title = path.parse(filepath).name;
-
-	// see http://www.regexr.com to test this regex
-	result.year = result.title.match(/(19|20)[0-9]{2}/gi);
-	if (result.year) {
-		result.year = result.year[0];
 	}
-	result.title = result.title.replace(/[\(|\.| ]*([19|20][0-9]{2}|dvdrip|french|1080p|x264|web-dl|bluray|brrip|720p)[\)|\.| ]*.*/gi, ""); // delete everything after year
-	result.title = result.title.replace(/\./gi, " "); // replace dot per space
-	result.title = result.title.replace(/ + /gi, " "); // remove multiple space
-	result.title = result.title.replace(/^ /gi, ""); // remove first space
-	result.title = result.title.replace(/ $/gi, ""); // remove last space
 
-	if (apiused === "imdb") {
-		getMovieImdb(result.title, result.year, callback);
-	} else {
-		getMovieAllocine(result.title, result.year, callback);
-	}
-}
-
-function getMovieImdb(title, year, callback) {
-	let result = {};
-	imdb.get(title, (err, res) => {
-		if (err) {
-			if (fallbackapi) {
-				getMovieAllocine(title, year, callback);
-			} else {
-				result.title = title;
-				return callback(err, result);
-			}
-		} else {
-			return callback(null, res);
-		}
-	});
-}
-
-function getMovieAllocine(title, year, callback) {
-	let result = {};
-	allocine.api('search', {q: title, filter: 'movie', count: 5}, (err, res) => {
-		if (res.feed.movie) {
-			result = changeMovieInfos(title, year, res);
-			return callback(null, result);
-		}
-		result.title = title;
-		return callback(err, result);
-	});
-}
-
-function createMovieAllocine(title, res, i) {
-	let result = {};
-	result.title = title;
-
-	result.title = res.feed.movie[i].title ? res.feed.movie[i].title : res.feed.movie[i].originalTitle;
-	result.year = res.feed.movie[i].productionYear;
-	result.poster = res.feed.movie[i].poster ? res.feed.movie[i].poster.href : "";
-	result.rating = res.feed.movie[i].statistics ? (parseFloat(res.feed.movie[i].statistics.userRating) * 2).toFixed(1) : "N/A";
-
-	return result;
-}
-
-function changeMovieInfos(title, year, res) {
-	let result = {};
-	result.title = title;
-
-	if (res.feed.movie.length > 1) {
-		for (let i = 0; i < res.feed.movie.length; i++) {
-			if (
-				parseInt(res.feed.movie[i].productionYear, 10) === parseInt(year, 10) &&
-				(
-					res.feed.movie[i].originalTitle.toLowerCase() === title.toLowerCase() ||
-					(
-						res.feed.movie[i].title &&
-						res.feed.movie[i].title.toLowerCase() === title.toLowerCase()
-					)
-				) ||
-				!year &&
-				(
-					res.feed.movie[i].originalTitle.toLowerCase() === title.toLowerCase() ||
-					(
-						res.feed.movie[i].title &&
-						res.feed.movie[i].title.toLowerCase() === title.toLowerCase()
-					)
-				)
-			) {
-				result = createMovieAllocine(title, res, i);
-				return result;
-			}
-		}
-	} else {
-		if (res.feed.movie[defaultindex]) {
-			result = createMovieAllocine(title, res, defaultindex);
-		}
-	}
-	return result;
-}
-
-function readdirectory(folder) {
-	movies = [];
-	moviespath = [];
-
-	readfolder(folder, (err, res) => {
-		flag = false;
-		filtermovies(res, (err, res) => {
+	// read folder recursively to get files in directory and sub directory
+	function readFolder(folder, callback) {
+		let results = [];
+		fs.readdir(folder, (err, files) => {
 			let pending;
-			moviespath = res;
-			pending = moviespath.length;
-			moviespath.forEach((element) => {
-				getMovie(element, (err, res) => {
-					pending--;
-					movies.push(res);
-					if (!pending) {
-						renderMovie();
+			if (err) {
+				return callback(err);
+			}
+			pending = files.length;
+			if (!pending) {
+				return callback(null, results);
+			}
+			files.forEach((file) => {
+				file = path.resolve(folder, file);
+				fs.stat(file, (err, stat) => {
+					if (stat && stat.isDirectory()) {
+						readFolder(file, (err, res) => {
+							pending--;
+							results = results.concat(res);
+							if (!pending) {
+								callback(null, results);
+							}
+						});
+					} else {
+						results.push(file);
+						pending--;
+						if (!pending) {
+							callback(null, results);
+						}
 					}
 				});
 			});
 		});
-	});
-}
+	}
 
-function getPreviousConfig() {
-	fallbackapi = localStorage.getItem('apifallback');
-	sortasc = localStorage.getItem('asc');
-	apiused = localStorage.getItem('currentapi');
-	folder = localStorage.getItem('currentfolder');
-	sortused = localStorage.getItem('sortby');
-}
+	// keep in array only movies thanks to extensions
+	function filterMoviesFiles(array, callback) {
+		array = array.filter((element) => {
+			let flag = false;
+			options.extensions.forEach((extension) => {
+				// compare extensions from file to extensions allowed
+				if (path.extname(element) === extension) {
+					flag = true;
+				}
+			});
+			if (flag) {
+				return element;
+			}
+		});
+		return callback(null, array);
+	}
 
-window.onload = function () {
-	folder = getUserHome();
+	// get infos for a movie with API
+	function getMovieInfos(filepath, callback) {
+		let result = {title: path.parse(filepath).name};
 
-	getPreviousConfig();
+		// see http://www.regexr.com to test regex
+		result.year = result.title.match(/(19|20)[0-9]{2}/gi);
+		if (result.year) {
+			result.year = result.year[0];
+		}
+		result.title = result.title.replace(/[\(|\.| ]*([19|20][0-9]{2}|dvdrip|french|1080p|x264|web-dl|bluray|brrip|720p)[\)|\.| ]*.*/gi, ""); // remove everything after year or specific words
+		result.title = result.title.replace(/\./gi, " "); // replace dot per space
+		result.title = result.title.replace(/ + /gi, " "); // remove multiple space
+		result.title = result.title.replace(/^ /gi, ""); // remove first space
+		result.title = result.title.replace(/ $/gi, ""); // remove last space
 
-	renderLayout();
+		if (options.api === "imdb") {
+			getMovieInfosFromImdb(result.title, result.year, callback);
+		} else {
+			getMovieInfosFromAllocine(result.title, result.year, callback);
+		}
+	}
 
-	readdirectory(folder);
-};
+	// get infos for a movie with IMDB API
+	function getMovieInfosFromImdb(title, year, callback) {
+		let result = {};
+		imdb.get(title, (err, res) => {
+			if (err) {
+				if (options.fallback) {
+					getMovieInfosFromAllocine(title, year, callback);
+				} else {
+					result.title = title;
+					return callback(err, result);
+				}
+			} else {
+				return callback(null, res);
+			}
+		});
+	}
+
+	// get infos for a movie with Allocine API
+	function getMovieInfosFromAllocine(title, year, callback) {
+		let result = {};
+		allocine.api('search', {q: title, filter: 'movie', count: 5}, (err, res) => {
+			if (res.feed.movie) {
+				result = convertMovieInfosFromAllocine(title, year, res);
+				return callback(null, result);
+			}
+			result.title = title;
+			return callback(err, result);
+		});
+	}
+
+	// reformat data from allocine to get right movie infos and look like IMDB data
+	function convertMovieInfosFromAllocine(title, year, res) {
+		let movie = res.feed.movie;
+		let result = {title: title};
+
+		for (let i = 0; i < movie.length; i++) {
+			// if just one movie in result
+			// or if year doesn't present in filename on hard drive
+			// or year form data is same than year form filename
+			// and original title from data is same than title from filename
+			// or title from data exist and is same than title form filename
+			if (movie.length == 1 || !year || (
+					parseInt(movie[i].productionYear, 10) === parseInt(year, 10) && (
+						movie[i].originalTitle.toLowerCase() === title.toLowerCase() || (
+							movie[i].title && movie[i].title.toLowerCase() === title.toLowerCase()
+						)
+					)
+				)
+			) {
+				// Allocine's API have original title and title (often translation in french)
+				result.title = movie[i].title ? movie[i].title : movie[i].originalTitle;
+				result.year = movie[i].productionYear;
+				result.poster = movie[i].poster ? movie[i].poster.href : "";
+				// convert rating from Allocine to look like rating IMDB
+				result.rating = movie[i].statistics ? (parseFloat(movie[i].statistics.userRating) * 2).toFixed(1) : "N/A";
+				return result;
+			}
+		}
+		return result;
+	}
+
+	init();
+})();
